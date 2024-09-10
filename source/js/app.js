de408f789a335ef3a14e34e384d79bb9ca1295c3
Daniel Schultz <cyberschultzoficial@gmail.com>
2024-09-09 21:35:44 -0300

    Versão 1.0

---
+++ source/js/app.js
@@ -0,0 +1,37 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nenhum modelo encontrado!</p>"
+        return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    for (dado of dados) {
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+            <div class="item-resultado">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Saiba mais</a>
+            </div>
+            `;
+        }
+    }
+    
+    if (!resultados) {
+        resultados = "<p>Nenhum modelo encontrado!</p>";
+    }
+    section.innerHTML = resultados
+}
---
+++ source/js/dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Ninja 300",
+        descricao: "A Kawasaki Ninja 300 é uma motocicleta esportiva que conquistou o coração de muitos motociclistas, tanto iniciantes quanto experientes.Com design agressivo e aerodinâmico, a Ninja 300 exala esportividade e modernidade.Equipada com um potente motor bicilíndrico de 296cc, ela oferece um desempenho emocionante e ágil, ideal para quem busca adrenalina nas ruas ou nas pistas.Além disso, a Ninja 300 é conhecida por sua excelente manobrabilidade e freios eficientes, proporcionando uma experiência de pilotagem segura e divertida.Seu painel de instrumentos completo e a posição de pilotagem esportiva completam o pacote, tornando a Ninja 300 uma excelente opção para quem busca uma moto versátil e com um ótimo custo-benefício.",
+        link: "https://www.kawasakibrasil.com/pt-br/motocicletas/ninja/sport/ninja-300",
+        tags: "esportiva carenada"
+    },
+    {
+        titulo: "Ninja 400",
+        descricao: "A Kawasaki Ninja 400 é uma motocicleta esportiva que combina a agilidade e o prazer de pilotar com um design moderno e sofisticado. Equipada com um potente motor bicilíndrico de 399cc, a Ninja 400 oferece uma experiência de pilotagem emocionante tanto para iniciantes quanto para pilotos mais experientes. Seu chassi leve e ágil, aliado a uma suspensão ajustável, proporciona um controle preciso e inspira confiança em qualquer tipo de estrada. Com freios a disco nas duas rodas e ABS de série, a segurança está garantida, permitindo que você aproveite cada curva com total tranquilidade. A Ninja 400 é a escolha perfeita para quem busca uma moto esportiva versátil, econômica e com um visual marcante.",
+        link: "https://www.kawasakibrasil.com/pt-br/motocicletas/ninja/sport/ninja-400",
+        tags: ""
+    },
+    {
+        titulo: "Ninja 500",
+        descricao: "A Kawasaki Ninja 500 é uma motocicleta esportiva que combina estilo, desempenho e praticidade. Com um design agressivo e aerodinâmico, a Ninja 500 exala esportividade e modernidade. Equipada com um motor bicilíndrico de 451cc, ela oferece um torque robusto e linear, ideal para o uso urbano e viagens mais longas. Seu chassi leve e ágil, aliado a uma suspensão balanceada, proporciona uma pilotagem confortável e divertida. Além disso, a Ninja 500 conta com freios a disco nas duas rodas e ABS de série, garantindo segurança e controle em diversas situações. Se você busca uma moto esportiva versátil, econômica e com um visual marcante, a Ninja 500 é a escolha perfeita.",
+        link: "https://www.kawasakibrasil.com/pt-br/motocicletas/ninja/sport/ninja-500",
+        tags: ""
+    }
+];
+
